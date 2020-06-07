b3c094d6d4df5e76fa087f124d85d48644f4b376
Aqeel Ahmed <aqeely208@gmail.com>
2020-06-07 21:36:32 +0500

    updated 6
    up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div>
-     Aqeel Ahmed
+    Hello Worl Aqeel Ahmed
     </div>
   );
 }
